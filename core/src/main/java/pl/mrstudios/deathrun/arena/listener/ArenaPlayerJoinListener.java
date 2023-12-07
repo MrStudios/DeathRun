7878792db0301f7dfdc8a4e22118630219d866d7
Hubert Kuliniak <kuliniakhubert@gmail.com>
2023-12-07 17:18:07 +0100

    cleanup

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinListener.java
@@ -2,7 +2,6 @@ package pl.mrstudios.deathrun.arena.listener;
 
 import net.kyori.adventure.platform.bukkit.BukkitAudiences;
 import net.kyori.adventure.text.minimessage.MiniMessage;
-import org.bukkit.ChatColor;
 import org.bukkit.GameMode;
 import org.bukkit.Material;
 import org.bukkit.Server;
@@ -84,32 +83,6 @@ public class ArenaPlayerJoinListener implements Listener {
                         .build()
         );
 
-    }
-
-    protected String miniMessageToLegacy(String message) {
-        return ChatColor.translateAlternateColorCodes('&', message.replace("<red>", "&c")
-                .replace("<green>", "&a")
-                .replace("<yellow>", "&e")
-                .replace("<blue>", "&9")
-                .replace("<white>", "&f")
-                .replace("<black>", "&0")
-                .replace("<gray>", "&7")
-                .replace("<dark_gray>", "&8")
-                .replace("<gold>", "&6")
-                .replace("<dark_red>", "&4")
-                .replace("<dark_green>", "&2")
-                .replace("<dark_blue>", "&1")
-                .replace("<dark_aqua>", "&3")
-                .replace("<dark_purple>", "&5")
-                .replace("<aqua>", "&b")
-                .replace("<light_purple>", "&d")
-                .replace("<bold>", "&l")
-                .replace("<italic>", "&o")
-                .replace("<strikethrough>", "&m")
-                .replace("<underline>", "&n")
-                .replace("<reset>", "&r")
-                .replace("<magic>", "&k")
-                .replace("<b>", "&b"));
     }
 
 }
