b8cf481807dfd479e519ab12b709e710b579f10f
Hubert Kuliniak <kuliniakhubert@gmail.com>
2023-12-07 13:18:56 +0100

    fixed players is able to join after game start

--- core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinKickListener.java
+++ core/src/main/java/pl/mrstudios/deathrun/arena/listener/ArenaPlayerJoinKickListener.java
@@ -4,13 +4,19 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerLoginEvent;
 import pl.mrstudios.commons.inject.annotation.Inject;
+import pl.mrstudios.deathrun.api.arena.enums.GameState;
+import pl.mrstudios.deathrun.arena.Arena;
 import pl.mrstudios.deathrun.arena.listener.annotations.ArenaRegistrableListener;
 
 @ArenaRegistrableListener
 public class ArenaPlayerJoinKickListener implements Listener {
 
+    private final Arena arena;
+
     @Inject
-    public ArenaPlayerJoinKickListener() {}
+    public ArenaPlayerJoinKickListener(Arena arena) {
+        this.arena = arena;
+    }
 
     @EventHandler
     public void onPlayerKick(PlayerLoginEvent event) {
@@ -18,6 +24,18 @@ public class ArenaPlayerJoinKickListener implements Listener {
         if (event.getResult() == PlayerLoginEvent.Result.KICK_FULL)
             if (event.getPlayer().hasPermission("mrstudios.deathrun.admin"))
                 event.allow();
+
+    }
+
+    @Deprecated
+    @EventHandler
+    public void onPlayerLogin(PlayerLoginEvent event) {
+
+        if (this.arena.getGameState() != GameState.WAITING && this.arena.getGameState() != GameState.STARTING)
+            event.disallow(PlayerLoginEvent.Result.KICK_FULL, "");
+
+        if (event.getPlayer().hasPermission("mrstudios.deathrun.admin"))
+            event.allow();
 
     }
 
