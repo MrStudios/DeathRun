485e6e2f7cd95909f617c1a567a0f4e22e165ed2
Hubert Kuliniak <kuliniakhubert@gmail.com>
2023-12-08 11:26:50 +0100

    fixed nullpointer exception

--- core/src/main/java/pl/mrstudios/deathrun/builder/ItemBuilder.java
+++ core/src/main/java/pl/mrstudios/deathrun/builder/ItemBuilder.java
@@ -1,20 +1,15 @@
 package pl.mrstudios.deathrun.builder;
 
-import com.destroystokyo.paper.profile.PlayerProfile;
-import com.destroystokyo.paper.profile.ProfileProperty;
 import net.kyori.adventure.text.minimessage.MiniMessage;
 import net.kyori.adventure.text.serializer.bungeecord.BungeeComponentSerializer;
 import net.md_5.bungee.api.chat.BaseComponent;
-import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.inventory.ItemFlag;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
-import org.bukkit.inventory.meta.SkullMeta;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.UUID;
 
 public class ItemBuilder {
 
@@ -32,11 +27,18 @@ public class ItemBuilder {
     }
 
     public ItemBuilder name(String name) {
+
+        if (this.itemMeta == null)
+            return this;
+
         this.itemMeta.setDisplayNameComponent(this.removeItalic(bungeeComponentSerializer.serialize(miniMessage.deserialize(name))));
         return this;
     }
 
     public ItemBuilder lore(List<String> lore) {
+
+        if (this.itemMeta == null)
+            return this;
 
         List<BaseComponent[]> components = new ArrayList<>();
 
@@ -53,13 +55,21 @@ public class ItemBuilder {
     }
 
     public ItemBuilder itemFlag(ItemFlag... itemFlags) {
+
+        if (this.itemMeta == null)
+            return this;
+
         this.itemMeta.addItemFlags(itemFlags);
         return this;
     }
 
     public ItemStack build() {
-        this.itemStack.setItemMeta(this.itemMeta);
+
+        if (this.itemMeta != null)
+            this.itemStack.setItemMeta(this.itemMeta);
+        
         return this.itemStack;
+
     }
 
     protected BaseComponent[] removeItalic(BaseComponent[] components) {
